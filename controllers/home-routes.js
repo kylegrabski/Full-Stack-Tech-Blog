e0148d8d339cfe98045692bfce9770e574b768ba
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-04 21:50:11 -0700

    show all post from api

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,23 +1,60 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models");
 
+//Hard Coded user for testing
+// const users = [
+//     {
+//         id: 1,
+//         name: "Kyle",
+//         email: "kyle.grabski@gmail.com",
+//         password: "password123"
+//     }
+// ]
 
-const users = [
-    {
-        id: 1,
-        name: "Kyle",
-        email: "kyle.grabski@gmail.com",
-        password: "password123"
-    }
-]
+//Testing to get hardcoded user
+// router.get('/', async (req, res) => {
+//     res.render('all-posts', {users});
+//   });
 
-router.get('/', async (req, res) => {
-    res.render('all-posts', {users});
-  });
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      //   include: [
+      //     {
+      //       model: User,
+      //     },
+      //   ],
+    });
 
+    const posts = dbPostData.map((content) => content.get({ plain: true }));
 
-router.get('/single-post/:num', async (req, res) => {
-    res.render('single-post', users[req.params.num -1]);
-  });
+    res.render("all-posts", {
+      posts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// render selected post
+router.get("/single-post/:num", async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.num);
+
+    const post = dbPostData.get({ plain: true });
+
+    JSON.stringify(post);
+
+    res.render("single-post", {
+      post,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
   
-  
-  module.exports = router;
+});
+
+module.exports = router;
