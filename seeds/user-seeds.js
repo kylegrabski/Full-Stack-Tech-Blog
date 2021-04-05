7c416e9a8ea6db4ebcaac4e325638c4cbc51f6a0
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-04 20:25:32 -0700

    create user seed

---
+++ seeds/user-seeds.js
@@ -0,0 +1,23 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        user_name: 'Kyle Grabski',
+        email: 'kyle.grabski@gmail.com',
+        password: 'test1234'
+    },
+    {
+        user_name: 'Oliver Cat',
+        email: 'oliver.cat@aol.com',
+        password: 'kitten1234'
+    },
+    {
+        user_name: 'Gordon Ramsey',
+        email: 'elGordo@netscape.com',
+        password: 'tortuga1234'
+    },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
