161bb8f33a95e918e57e70a7016e4145da2ab4e6
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-04 20:38:43 -0700

    create post seed

---
+++ seeds/post-seeds.js
@@ -0,0 +1,23 @@
+const { Post } = require('../models');
+
+const postData = [
+    {
+        title: "Kyle's First Post Title",
+        content: "Hello! My name is Kyle, and this is my first post content!",
+        user_id: 1,
+    },
+    {
+        title: "Olivers First Post Title",
+        content: "Hello! My name is Oliver, and this is my first post content!",
+        user_id: 2,
+    },
+    {
+        title: "Gordon's First Post Title",
+        content: "Hello! My name is Gordon, and this is my first post content!",
+        user_id: 3,
+    },
+];
+
+const seedPosts = () => Post.bulkCreate(postData);
+
+module.exports = seedPosts;
