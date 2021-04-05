d3bbf0c06d7c8cb79a7f7c3c076bc5776d009698
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-04 20:32:51 -0700

    update comment/post association

--- models/index.js
+++ models/index.js
@@ -10,12 +10,22 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-User.hasMany(Comment, {
-  foreignKey: "user_id",
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
 });
 
-Comment.belongsTo(User, {
-  foreignKey: "user_id",
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
 });
+
+
+// User.hasMany(Comment, {
+//   foreignKey: "user_id",
+// });
+
+// Comment.belongsTo(User, {
+//   foreignKey: "user_id",
+// });
 
 module.exports = { User, Post, Comment };
