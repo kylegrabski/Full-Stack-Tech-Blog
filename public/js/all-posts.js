8d657b4e7b9a9615707ef36b03aeb4b18bdae3c2
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-05 21:40:24 -0700

    add event listener to new post button

--- public/js/all-posts.js
+++ public/js/all-posts.js
@@ -1,10 +1,15 @@
 const $commentBtn = document.querySelectorAll("#commentBtn");
+const $postBtn = document.querySelector("#postBtn");
 
-$commentBtn.forEach(item => {
+$commentBtn.forEach((item) => {
   item.addEventListener("click", function (e) {
     console.log(this.value);
     if (e) {
       document.location.replace(`/single-post/${this.value}`);
     }
-  })
+  });
+});
+
+$postBtn.addEventListener("click", (e) => {
+  document.location.replace("/new-post");
 });
