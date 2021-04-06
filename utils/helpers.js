cc0fc33f1523ba8bdd9939c4528a8991558568fc
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-05 20:14:35 -0700

    working on format date helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,9 @@
-module.exports = {
-    // the helper method 'format_time' will take in a timestamp and return a string with only the time
-    format_time: (date) => {
-      // We use the 'toLocaleTimeString()' method to format the time as H:MM:SS AM/PM
-      return date.toLocaleTimeString();
-    },
-  };
+// module.exports = {
+//     // the helper method 'format_time' will take in a timestamp and return a string with only the time
+//     format_time: (date) => {
+//       // We use the 'toLocaleTimeString()' method to format the time as H:MM:SS AM/PM
+//       return date.toLocaleTimeString();
+//     },
+//   };
+
+//   probably the get date and get time instead of toLocaleTimeString
